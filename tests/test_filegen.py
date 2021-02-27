721891125193ccca79887103dbb95ca25d629de4
DM <devnull@localhost>
2021-02-27 15:45:43 -0500

    Start tests with a filegen test.

---
+++ tests/test_filegen.py
@@ -0,0 +1,13 @@
+import unittest
+
+from dmgen import filegen
+
+
+class FilegenTest(unittest.TestCase):
+    def test_extensionis(self):
+        self.assertEqual(filegen.extensionis('file.txt', 'txt'), True)
+        self.assertEqual(filegen.extensionis('file.txt', '.txt'), True)
+
+
+if __name__ == "__main__":
+    unittest.main()
