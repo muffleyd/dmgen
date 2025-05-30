7a82c7ecc463ed6f4e42d4774b6cf2a7e6bd381e
DM <devnull@localhost>
2025-05-29 21:02:34 -0400

    Split out gen.timer to timer.Timer

--- gen.py
+++ gen.py
@@ -11,6 +11,7 @@ import threading
 import queue
 import functools
 from collections import deque
+from . import timer
 
 monotonic = time.monotonic
 
@@ -655,44 +656,10 @@ def _return_print_dict(dictionary, spaces=0, between='\n'):
     ) for key, value in dictionary.items()])
 
 
-class timer:
-    def __init__(self, do_print=True, decimals=3, newline=True, before='', after=''):
-        self.do_print = do_print
-        self.before_print = before
-        self.decimals_print = decimals
-        self.after_print = after
-        self.newline = newline
-
-    def get_runtime(self):
-        if self.runtime is None:
-            raise ValueError('timer still running')
-        return self.runtime
-
-    def build_print_string(self):
-        # Format runtime to decimal length.
-        runtime_fstring = f'{self.runtime:.{self.decimals_print}f}'
-        # Format the rest of the string.
-        return f'{self.before_print}{runtime_fstring}{self.after_print}'
-
-    def print_me(self, newline=True):
-        to_print = self.build_print_string()
-        if newline:
-            print(to_print)
-        else:
-            print(to_print, end=' ')
-
-    def __repr__(self):
-        return str(self.get_runtime())
-
-    def __enter__(self):
-        self.runtime = None
-        self.start = monotonic()
-        return self
-
-    def __exit__(self, *exc):
-        self.runtime = monotonic() - self.start
-        if self.do_print:
-            self.print_me()
+def timer(*args, **kwargs):
+    import warnings
+    warnings.warn('gen.timer is deprecated, use timer.Timer')
+    return timer.Timer(*args, **kwargs)
 
 
 def menu(title, options=None, question=None, numbers=0, format=1, mod=1, entry=None):
---
+++ timer.py
@@ -0,0 +1,41 @@
+from time import monotonic
+
+class Timer:
+    def __init__(self, do_print=True, decimals=3, newline=True, before='', after=''):
+        self.do_print = do_print
+        self.before_print = before
+        self.decimals_print = decimals
+        self.after_print = after
+        self.newline = newline
+        self.runtime = None
+
+    def get_runtime(self):
+        if self.runtime is None:
+            raise ValueError('timer still running')
+        return self.runtime
+
+    def build_print_string(self):
+        # Format runtime to decimal length.
+        runtime_fstring = f'{self.runtime:.{self.decimals_print}f}'
+        # Format the rest of the string.
+        return f'{self.before_print}{runtime_fstring}{self.after_print}'
+
+    def print_me(self, newline=True):
+        to_print = self.build_print_string()
+        if newline:
+            print(to_print)
+        else:
+            print(to_print, end=' ')
+
+    def __repr__(self):
+        return str(self.get_runtime())
+
+    def __enter__(self):
+        self.runtime = None
+        self.start = monotonic()
+        return self
+
+    def __exit__(self, *exc):
+        self.runtime = monotonic() - self.start
+        if self.do_print:
+            self.print_me()
