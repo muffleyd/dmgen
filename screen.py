cf81dabb7f580008257fc9d31206f9a21aca6d5b
DM <devnull@localhost>
2025-10-19 21:00:06 -0400

    Add screen function for X-Windows

--- screen.py
+++ screen.py
@@ -10,6 +10,17 @@ def main():
     return width, height
 
 
+
+def main_linux_x():
+    import subprocess
+    output = subprocess.Popen('xrandr | grep "\\*" | cut -d" " -f4',shell=True, stdout=subprocess.PIPE).communicate()[0]
+    if isinstance(output, bytes):
+        output = output.decode('utf-8')
+    main_monitor = output.split('\n')[0]
+    width, height = main_monitor.split('x')
+    return int(width), int(height)
+
+
 # untested
 def main_mac():
     import AppKit
@@ -24,10 +35,31 @@ def main_win():
             user32.GetSystemMetrics(76), user32.GetSystemMetrics(77))
 
 
-try:
-    WIDTH, HEIGHT, MONITOR2_X, MONITOR2_Y = main_win()
-except:
-    try:
-        WIDTH, HEIGHT = main_mac()
-    except:
-        WIDTH, HEIGHT = main()
+def main():
+    import sys
+    global WIDTH, HEIGHT, MONITOR2_X, MONITOR2_Y
+    mapping = {
+        'linux': main_linux_x,
+        'win32': main_win,
+        'darwin': main_mac
+    }
+    if function := mapping.get(sys.platform):
+        functions = [function, main]
+    else:
+        functions = [main_win, main_mac, main_linux_x, main]
+    result = None
+    for function in functions:
+        try:
+            result = function()
+        except:
+            continue
+        else:
+            break
+    if not result:
+        raise Exception('Could not determine monitor resolution.')
+    WIDTH, HEIGHT = result[:2]
+    if len(result) == 4:
+        MONITOR2_X, MONITOR2_Y = result[2:4]
+
+
+main()
